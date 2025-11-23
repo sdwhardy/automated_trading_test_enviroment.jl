3f8bdf5cb520707ea951ab6d3b4581ac8a7629a1
sdwhardy <steve_dw_hardy@yahoo.ca>
2025-11-23 06:45:49 +0100

    data retrieval basics done

--- src/data.jl
+++ src/data.jl
@@ -35,27 +35,30 @@ end
 #Close - the final traded price
 #Volume - the total volume traded by all trades
 #Trades - the number of individual trades
-#1, 5, 15, 30, 60, 240, 720 and 1440 minute intervals
-#Data spans: 2015-08-07T14:03:00+02:00 - 2025-03-31T23:59:00+02:00
+#1, 5, 15, 30, 60, 240, 720 (12 hour) and 1440 (24 hour) minute intervals
+#BTC Data spans: 2013-10-06T21:34:00+02:00 - 2025-03-31T23:59:00+02:00
+#ETH Data spans: 2015-08-07T14:03:00+02:00 - 2025-03-31T23:59:00+02:00
 
+OHLCVT=Dict()
 pair="ETHUSD"
-interval="5"
-
-ETH_df=get_pair_interval_df(pair,interval)
+interval="1440"
+push!(OHLCVT,pair=>Dict(interval=>get_pair_interval_df(pair,interval)))
+interval="720"
+push!(OHLCVT[pair],interval=>get_pair_interval_df(pair,interval))
     
 
 
-ts = ETH_df[!,:timestamp][1]
+
+pair="XBTUSD"
+interval="1440"
+push!(OHLCVT,pair=>Dict(interval=>get_pair_interval_df(pair,interval)))
+interval="720"
+push!(OHLCVT[pair],interval=>get_pair_interval_df(pair,interval))
+
+
+
+#=ts = OHLCVT["ETHUSD"]["1440"][!,:timestamp][end]
 dt = unix2datetime(ts)
 dt_be = ZonedDateTime(dt, tz"Europe/Brussels")
-println(dt_be)
-
-pair="BTCUSD"
-interval="5"
-
-BTC_df=get_pair_interval_df(pair,interval)
-ts = BTC_df[!,:timestamp][1]
-dt = unix2datetime(ts)
-dt_be = ZonedDateTime(dt, tz"Europe/Brussels")
-println(dt_be)
+println(dt_be)=#
     
